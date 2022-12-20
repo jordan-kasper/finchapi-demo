2c0387b89190a0492465648052919c2ca6cbe809
Jordan Kasper <jordan-mbp@macbook-pro.local>
2022-12-20 13:52:27 -0800

    adding benefits data

--- webapp.py
+++ webapp.py
@@ -9,79 +9,165 @@ import sys
 app = Flask(__name__)
 
 global token
+global ben_id
 
 @app.route("/")
 def landing_page():
     context = {'project': "flask-bootstrap", 'author': "Anubhav Sinha", 'items':["python", "flask", "jinja2", "bootstrap", "font-awesome", "jquery"] }
+
+    global token
+    token = ''
+    
     return render_template('landing_page.html', **context)
 
 @app.route('/people')
 
 def getPeople():
-    
-    print(token)
-    conn = http.client.HTTPSConnection("api.tryfinch.com")
+    if token == '':
+            return render_template('notoken.html')
+    else:    
+        print(token)
+        conn = http.client.HTTPSConnection("api.tryfinch.com")
 
-    headers = {
-        'Content-Type': "application/json",
-        'Finch-API-Version': "2020-09-17",
-        'Authorization': "Bearer "+token+""
-        }
+        headers = {
+            'Content-Type': "application/json",
+            'Finch-API-Version': "2020-09-17",
+            'Authorization': "Bearer "+token+""
+            }
 
-    conn.request("GET", "/employer/directory", headers=headers)
+        conn.request("GET", "/employer/directory", headers=headers)
 
-    res = conn.getresponse()
+        res = conn.getresponse()
 
-    data = res.read()
-    
-    json_data = json.loads(data)
+        data = res.read()
+        
+        json_data = json.loads(data)
 
-    return render_template('directory.html',data=json_data)
+        return render_template('directory.html',data=json_data)
 
 @app.route('/company')
 
 def getCompany():
 
-    conn = http.client.HTTPSConnection("api.tryfinch.com")
+    if token == '':
+        return render_template('notoken.html')
+    else:
+        conn = http.client.HTTPSConnection("api.tryfinch.com")
 
-    headers = {
-        'Content-Type': "application/json",
-        'Finch-API-Version': "2020-09-17",
-        'Authorization': "Bearer "+token+""
-        }
+        headers = {
+            'Content-Type': "application/json",
+            'Finch-API-Version': "2020-09-17",
+            'Authorization': "Bearer "+token+""
+            }
 
-    conn.request("GET", "/employer/company", headers=headers)
+        conn.request("GET", "/employer/company", headers=headers)
 
-    res = conn.getresponse()
-    data = res.read()
+        res = conn.getresponse()
+        data = res.read()
 
-    json_data = json.loads(data)
+        json_data = json.loads(data)
 
-    return render_template('company.html',data=json_data)
+        return render_template('company.html',data=json_data)
 
 @app.route('/payroll')
 
-
-
 def getPayroll():
+    if token == '':
+            return render_template('notoken.html')
+    else:
+            def payroll():
 
-    conn = http.client.HTTPSConnection("finch-sandbox.vercel.app")
+                conn = http.client.HTTPSConnection("finch-sandbox.vercel.app")
 
-    headers = {
-        'Content-Type': "application/json",
-        'Finch-API-Version': "2020-09-17",
-        'Authorization': "Bearer sandbox-token-c5777705-0253-4497-b7f9-db7548153739"
-        }
+                headers = {
+                    'Content-Type': "application/json",
+                    'Finch-API-Version': "2020-09-17",
+                    'Authorization': "Bearer sandbox-token-c5777705-0253-4497-b7f9-db7548153739"
+                    }
 
-    conn.request("GET", "/api/employer/payment?start_date=2021-12-01&end_date=2022-11-01", headers=headers)
+                conn.request("GET", "/api/employer/payment?start_date=2021-12-01&end_date=2022-11-01", headers=headers)
 
-    res = conn.getresponse()
+                res = conn.getresponse()
 
-    data = res.read()
-    
-    json_data = json.loads(data)
+                data = res.read()
+                
+                json_data = json.loads(data)
 
-    return render_template('payment.html',data=json_data)
+                return json_data
+
+            def people():
+                conn = http.client.HTTPSConnection("api.tryfinch.com")
+
+                headers = {
+                    'Content-Type': "application/json",
+                    'Finch-API-Version': "2020-09-17",
+                    'Authorization': "Bearer "+token+""
+                    }
+
+                conn.request("GET", "/employer/directory", headers=headers)
+
+                res = conn.getresponse()
+
+                data = res.read()
+                
+                json_data = json.loads(data)
+
+                return json_data
+
+            pr = payroll()
+            my_people = people()
+            return render_template('payment.html',data=pr, people=my_people)
+
+@app.route('/benefits')
+
+def getBenefits():
+    if token == '':
+            return render_template('notoken.html')
+    else:
+            def getBenefits():
+
+                conn = http.client.HTTPSConnection("api.tryfinch.com")
+
+                headers = {
+                    'Content-Type': "application/json",
+                    'Finch-API-Version': "2020-09-17",
+                    'Authorization': "Bearer "+token+""
+                    }
+
+                conn.request("GET", "/employer/benefits", headers=headers)
+
+                res = conn.getresponse()
+
+                data = res.read()
+                
+                json_data = json.loads(data)
+
+                return json_data
+
+            def people():
+                conn = http.client.HTTPSConnection("api.tryfinch.com")
+
+                headers = {
+                    'Content-Type': "application/json",
+                    'Finch-API-Version': "2020-09-17",
+                    'Authorization': "Bearer "+token+""
+                    }
+
+                conn.request("GET", "/employer/directory", headers=headers)
+
+                res = conn.getresponse()
+
+                data = res.read()
+                
+                json_data = json.loads(data)
+
+                return json_data
+
+
+            benefits = getBenefits()
+            my_people = people()
+
+            return render_template('benefits.html',data=benefits, people=my_people)
 
 @app.route('/getCode', methods=['POST'])
 
